27e0bea96d5e955a32ddff43cb3de1e4013150d0
waynetipsy <jacketoniru@gmail.com>
2022-07-19 03:28:55 +0100

    Final push

---
+++ lib/Model/data.dart
@@ -0,0 +1,36 @@
+class Data {
+  final String? title;
+  final int? followers;
+
+  Data({
+    this.title, this.followers});
+}
+
+List<Data> socialData = [
+  Data(
+    title: "Hello World",
+    followers: 45,
+  ),
+
+    Data(
+    title: "Data World",
+    followers: 45,
+  ),
+
+    Data(
+    title: "Twitter World",
+    followers: 45,
+  ),
+
+    Data(
+    title: "Meta Verse World",
+    followers: 45,
+  ),
+
+    Data(
+    title: "Instagram World",
+    followers: 45,
+  ),
+
+
+];
